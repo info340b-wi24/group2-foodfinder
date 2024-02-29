7b6430a0257b7933ac6af96e6a58d749d096f14d
kenx128 <kenx128@uw.edu>
2024-02-28 21:05:20 -0800

    added food card and logos, havent finish review part

---
+++ src/Components/FoodList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+//component representing the restaurant cards in the home page
+function FoodCard(props) {
+  return (
+    <div className="card">
+      <Link to="/restaurant"><img src={props.img} alt={props.name}/></Link>
+      <div className="card-title">
+          <h2>{props.name}</h2>
+          <h2 className="right grade">{props.rating}</h2>
+      </div>
+      <p>{props.type}</p>
+      <p>Cost: $</p>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+export default function FoodList(props) {
+  const cards = props.foodData.map((food, index) => {
+    return (
+      <FoodCard key={index} {...food}/>
+    )
+  });
+
+  return (
+    <div className="box food">
+      {cards}
+    </div>
+  )
+}
